07622540eecc1deadea78a571bab1e858b5b7737
juancruzcoca <juancruzcoca@gmail.com>
2025-04-25 15:08:46 -0300

    pdf maker complete

--- moin.py
+++ moin.py
@@ -13,8 +13,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="times", style="B", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align="L")
-    pdf.line(10, 21, 200, 21)
-    
+    # pdf.line(10, 21, 200, 21)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+
     # Set the footer
     pdf.ln(265)
     pdf.set_font(family="times", style="I", size=8)
@@ -28,4 +30,4 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-pdf.output("output.pdf")
+pdf.output("output_lined.pdf")
